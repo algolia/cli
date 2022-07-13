537ebe158d4dcc59cf5210d8695f0b99d9561bc3
Clément Denoix <flydesigned@gmail.com>
2022-07-13 21:05:49 +0200

    feat(pkg) Add telemetry (#12)

--- cmd/algolia/main.go
+++ cmd/algolia/main.go
@@ -1,121 +1,12 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"io"
-	"net"
 	"os"
-	"strings"
 
-	surveyCore "github.com/AlecAivazis/survey/v2/core"
-	"github.com/AlecAivazis/survey/v2/terminal"
-	"github.com/mgutz/ansi"
-	"github.com/spf13/cobra"
-
-	"github.com/algolia/cli/pkg/cmd/factory"
 	"github.com/algolia/cli/pkg/cmd/root"
-	"github.com/algolia/cli/pkg/cmdutil"
-	"github.com/algolia/cli/pkg/config"
-)
-
-type exitCode int
-
-const (
-	exitOK     exitCode = 0
-	exitError  exitCode = 1
-	exitCancel exitCode = 2
-	exitAuth   exitCode = 4
 )
 
 func main() {
-	code := mainRun()
+	code := root.Execute()
 	os.Exit(int(code))
-}
-
-func mainRun() exitCode {
-	hasDebug := os.Getenv("DEBUG") != ""
-
-	cfg := config.Config{}
-	cfg.InitConfig()
-	cmdFactory := factory.New(&cfg)
-
-	stderr := cmdFactory.IOStreams.ErrOut
-
-	if !cmdFactory.IOStreams.ColorEnabled() {
-		surveyCore.DisableColor = true
-	} else {
-		// override survey's poor choice of color
-		surveyCore.TemplateFuncsWithColor["color"] = func(style string) string {
-			switch style {
-			case "white":
-				if cmdFactory.IOStreams.ColorSupport256() {
-					return fmt.Sprintf("\x1b[%d;5;%dm", 38, 242)
-				}
-				return ansi.ColorCode("default")
-			default:
-				return ansi.ColorCode(style)
-			}
-		}
-	}
-
-	rootCmd := root.NewRootCmd(cmdFactory)
-
-	authError := errors.New("authError")
-	rootCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
-		// require that the user is authenticated before running most commands
-		if cmdutil.IsAuthCheckEnabled(cmd) {
-			if err := cmdutil.CheckAuth(cfg); err != nil {
-				fmt.Fprintf(stderr, "Authentication error: %s\n", err)
-				fmt.Fprintln(stderr, "Please run `algolia application add` to configure your first application.")
-				return authError
-			} else {
-				return nil
-			}
-		}
-
-		return nil
-	}
-
-	if cmd, err := rootCmd.ExecuteC(); err != nil {
-		if err == cmdutil.ErrSilent {
-			return exitError
-		} else if cmdutil.IsUserCancellation(err) {
-			if errors.Is(err, terminal.InterruptErr) {
-				// ensure the next shell prompt will start on its own line
-				fmt.Fprint(stderr, "\n")
-			}
-			return exitCancel
-		} else if errors.Is(err, authError) {
-			return exitAuth
-		}
-
-		printError(stderr, err, cmd, hasDebug)
-
-		return exitError
-	}
-
-	return exitOK
-}
-
-func printError(out io.Writer, err error, cmd *cobra.Command, debug bool) {
-	var dnsError *net.DNSError
-	if errors.As(err, &dnsError) {
-		fmt.Fprintf(out, "error connecting to %s\n", dnsError.Name)
-		if debug {
-			fmt.Fprintln(out, dnsError)
-		}
-		fmt.Fprintln(out, "check your internet connection or https://status.algolia.com")
-		return
-	}
-
-	fmt.Fprintln(out, err)
-
-	var flagError *cmdutil.FlagError
-	if errors.As(err, &flagError) || strings.HasPrefix(err.Error(), "unknown command ") {
-		if !strings.HasSuffix(err.Error(), "\n") {
-			fmt.Fprintln(out)
-		}
-		fmt.Fprintln(out, cmd.UsageString())
-	}
 }
