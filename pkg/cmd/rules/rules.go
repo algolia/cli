74d9d41e9431e2eb231977628c7b9ac64f138a64
Clément Denoix <flydesigned@gmail.com>
2022-08-12 01:31:30 +0200

    Add missing AddCommand statements (#30)

--- pkg/cmd/rules/rules.go
+++ pkg/cmd/rules/rules.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/algolia/cli/pkg/cmd/rules/browse"
+	"github.com/algolia/cli/pkg/cmd/rules/delete"
 	importRules "github.com/algolia/cli/pkg/cmd/rules/import"
 	"github.com/algolia/cli/pkg/cmdutil"
 )
@@ -18,6 +19,7 @@ func NewRulesCmd(f *cmdutil.Factory) *cobra.Command {
 
 	cmd.AddCommand(importRules.NewImportCmd(f))
 	cmd.AddCommand(browse.NewBrowseCmd(f))
+	cmd.AddCommand(delete.NewDeleteCmd(f, nil))
 
 	return cmd
 }
