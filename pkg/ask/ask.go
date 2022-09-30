79b2b599055cf0fab59bd7889448ec5f1888d5bf
Loïc Say <loic.say@algolia.com>
2022-09-30 10:48:19 +0200

    chore: survey custom types

--- pkg/ask/ask.go
+++ pkg/ask/ask.go
@@ -2,35 +2,45 @@ package ask
 
 import (
 	"github.com/AlecAivazis/survey/v2"
+
 	"github.com/algolia/cli/pkg/utils"
 )
 
-func AskInputQuestion(message string, storage *string, defaultValue string, opts ...survey.AskOpt) error {
-	return survey.AskOne(&survey.Input{
-		Message: message,
-		Default: defaultValue,
-	}, storage, opts...)
+// https://github.com/AlecAivazis/survey#custom-types
+type StringSlice struct {
+	value []string
+}
+
+func (my *StringSlice) WriteAnswer(name string, value interface{}) error {
+	my.value = utils.StringToSlice(value.(string))
+	return nil
 }
 
 func AskCommaSeparatedInputQuestion(message string, storage *[]string, defaultValues []string, opts ...survey.AskOpt) error {
-	values := ""
+	stringSlice := StringSlice{}
+	err := survey.AskOne(
+		&survey.Input{
+			Message: message,
+			Default: utils.SliceToString(defaultValues),
+		},
+		&stringSlice,
+	)
+	*storage = stringSlice.value
 
-	err := survey.AskOne(&survey.Input{
-		Message: message,
-		Default: utils.SliceToString(defaultValues),
-	}, &values, opts...)
-	if err != nil {
-		return err
-	}
-
-	*storage = utils.StringToSlice(values)
-	return nil
+	return err
 }
 
 func AskSelectQuestion(message string, storage *string, options []string, defaultValue string, opts ...survey.AskOpt) error {
 	return survey.AskOne(&survey.Select{
 		Message: message,
 		Options: options,
+		Default: defaultValue,
+	}, storage, opts...)
+}
+
+func AskInputQuestion(message string, storage *string, defaultValue string, opts ...survey.AskOpt) error {
+	return survey.AskOne(&survey.Input{
+		Message: message,
 		Default: defaultValue,
 	}, storage, opts...)
 }
