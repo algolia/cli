a4ce6e248b000a40782caa1fe29dc4e7a5ef99c7
Loïc Say <loic.say@gmail.com>
2022-10-05 17:30:04 +0200

    chore: rename branch master to main (#67)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -7,8 +7,8 @@ import (
 // Version of the CLI.
 // This is set to the actual version by GoReleaser, identify by the
 // git tag assigned to the release. Versions built from source will
-// always show master.
-var Version = "master"
+// always show main.
+var Version = "main"
 
 // Template for the version string.
 var Template = fmt.Sprintf("algolia version %s\n", Version)
