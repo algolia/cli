5474743090276f5f52cf31831eeb3d331856c59f
Clément Denoix <flydesigned@gmail.com>
2022-12-06 10:27:40 +0100

    [DEX-315] Dictionary extended help (#91)

--- pkg/cmd/dictionary/dictionary.go
+++ pkg/cmd/dictionary/dictionary.go
@@ -1,6 +1,7 @@
 package dictionary
 
 import (
+	"github.com/MakeNowJust/heredoc"
 	"github.com/spf13/cobra"
 
 	"github.com/algolia/cli/pkg/cmd/dictionary/entries"
@@ -14,6 +15,23 @@ func NewDictionaryCmd(f *cmdutil.Factory) *cobra.Command {
 		Use:     "dictionary",
 		Aliases: []string{"dictionaries"},
 		Short:   "Manage your Algolia dictionaries",
+		Annotations: map[string]string{
+			"help:see-also": heredoc.Doc(`
+			The below command examples are not directly related to the dictionary command, but are relevant to the use of dictionaries in general.
+
+			# Open Algolia supported languages page
+			$ algolia open languages
+
+			# Set the 'ignorePlurals' setting to true: https://www.algolia.com/doc/api-reference/api-parameters/ignorePlurals/
+			$ algolia settings set <index> --ignorePlurals
+
+			# Set the 'removeStopWords' setting to true: https://www.algolia.com/doc/api-reference/api-parameters/removeStopWords/
+			$ algolia settings set <index> --removeStopWords
+
+			# Set the 'decompoundQuery' setting to true: https://www.algolia.com/doc/api-reference/api-parameters/decompoundQuery/
+			$ algolia settings set <index> --decompoundQuery
+			`),
+		},
 	}
 
 	cmd.AddCommand(settings.NewSettingsCmd(f))
