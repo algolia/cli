f9336b6b3732ef84ec33a1d212360c8d478cb13a
Dylan Tientcheu <dylantientcheu@gmail.com>
2023-01-13 08:55:32 +0100

    [DEX-309] algolia dictionary entries delete (#96)
    * feat: adds algolia delete dictionary entries

---
+++ pkg/cmd/dictionary/entries/delete/delete_test.go
@@ -0,0 +1,175 @@
+package delete
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/algolia/algoliasearch-client-go/v3/algolia/search"
+	"github.com/google/shlex"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/algolia/cli/pkg/cmdutil"
+	"github.com/algolia/cli/pkg/httpmock"
+	"github.com/algolia/cli/pkg/iostreams"
+	"github.com/algolia/cli/test"
+)
+
+func TestNewDeleteCmd(t *testing.T) {
+	tests := []struct {
+		name      string
+		tty       bool
+		cli       string
+		wantsErr  bool
+		wantsOpts DeleteOptions
+	}{
+		{
+			name:     "no --confirm without tty",
+			cli:      "plural --object-ids 1",
+			tty:      false,
+			wantsErr: true,
+		},
+		{
+			name:     "--confirm without tty",
+			cli:      "plural --object-ids 1 --confirm",
+			tty:      true,
+			wantsErr: false,
+			wantsOpts: DeleteOptions{
+				DoConfirm:   false,
+				Dictionnary: "plural",
+				ObjectIDs: []string{
+					"1",
+				},
+			},
+		},
+		{
+			name:     "no --confirm with tty",
+			cli:      "plural --object-ids 1",
+			tty:      true,
+			wantsErr: false,
+			wantsOpts: DeleteOptions{
+				DoConfirm:   true,
+				Dictionnary: "plural",
+				ObjectIDs: []string{
+					"1",
+				},
+			},
+		},
+		{
+			name:     "multiple --object-ids",
+			cli:      "plural --object-ids 1,2 --confirm",
+			tty:      false,
+			wantsErr: false,
+			wantsOpts: DeleteOptions{
+				DoConfirm:   false,
+				Dictionnary: "plural",
+				ObjectIDs: []string{
+					"1",
+					"2",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			io, _, stdout, stderr := iostreams.Test()
+			if tt.tty {
+				io.SetStdinTTY(tt.tty)
+				io.SetStdoutTTY(tt.tty)
+			}
+
+			f := &cmdutil.Factory{
+				IOStreams: io,
+			}
+
+			var opts *DeleteOptions
+			cmd := NewDeleteCmd(f, func(o *DeleteOptions) error {
+				opts = o
+				return nil
+			})
+
+			args, err := shlex.Split(tt.cli)
+			require.NoError(t, err)
+			cmd.SetArgs(args)
+			_, err = cmd.ExecuteC()
+			if tt.wantsErr {
+				assert.Error(t, err)
+				return
+			} else {
+				require.NoError(t, err)
+			}
+
+			assert.Equal(t, "", stdout.String())
+			assert.Equal(t, "", stderr.String())
+
+			assert.Equal(t, tt.wantsOpts.Dictionnary, opts.Dictionnary)
+			assert.Equal(t, tt.wantsOpts.ObjectIDs, opts.ObjectIDs)
+			assert.Equal(t, tt.wantsOpts.DoConfirm, opts.DoConfirm)
+		})
+	}
+}
+
+func Test_runDeleteCmd(t *testing.T) {
+	tests := []struct {
+		name        string
+		cli         string
+		dictionnary string
+		objectIDs   []string
+		isTTY       bool
+		wantOut     string
+	}{
+		{
+			name:        "single object-id, no TTY",
+			cli:         "plural --object-ids 1 --confirm",
+			dictionnary: "plural",
+			objectIDs: []string{
+				"1",
+			},
+			isTTY:   false,
+			wantOut: "",
+		},
+		{
+			name:        "single object-id, TTY",
+			cli:         "plural --object-ids 1 --confirm",
+			dictionnary: "plural",
+			objectIDs: []string{
+				"1",
+			},
+			isTTY:   true,
+			wantOut: "✓ Successfully deleted 1 object from plural\n",
+		},
+		{
+			name:        "multiple object-ids, TTY",
+			cli:         "plural --object-ids 1,2 --confirm",
+			dictionnary: "plural",
+			objectIDs: []string{
+				"1",
+				"2",
+			},
+			isTTY:   true,
+			wantOut: "✓ Successfully deleted 2 objects from plural\n",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := httpmock.Registry{}
+
+			// test is flaky since there's no guarantee of obtaining the right object using a search by objectID
+			for _, id := range tt.objectIDs {
+				r.Register(httpmock.REST("GET", fmt.Sprintf("1/dictionaries/%s/search?query=%s", tt.dictionnary, id)), httpmock.JSONResponse(search.SearchDictionariesRes{}))
+			}
+			r.Register(httpmock.REST("POST", fmt.Sprintf("1/dictionaries/%s/batch", tt.dictionnary)), httpmock.JSONResponse(search.TaskStatusRes{}))
+
+			f, out := test.NewFactory(tt.isTTY, &r, nil, "")
+			cmd := NewDeleteCmd(f, nil)
+			out, err := test.Execute(cmd, tt.cli, out)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			assert.Equal(t, tt.wantOut, out.String())
+		})
+	}
+}
