5876077761f4e3a7b4879d83ee51af2cecde9c3b
Dylan Tientcheu <dylantientcheu@gmail.com>
2023-01-26 12:17:32 +0100

    fix: lint

--- pkg/iostreams/console.go
+++ pkg/iostreams/console.go
@@ -9,7 +9,7 @@ func (s *IOStreams) EnableVirtualTerminalProcessing() error {
 	return nil
 }
 
-func hasAlternateScreenBuffer(hasTrueColor bool) bool {
+func HasAlternateScreenBuffer(hasTrueColor bool) bool {
 	// on Windows we just assume that alternate screen buffer is supported if we
 	// enabled virtual terminal processing, which in turn enables truecolor
 	return hasTrueColor
