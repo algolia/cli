25bc6bc8591d28d0cc79252a52fd9d4e1efb0e43
Clement Denoix <flydesigned@gmail.com>
2024-03-25 22:18:52 +0100

    feat(cmd): Add apikeys get command

--- pkg/cmd/apikeys/apikeys.go
+++ pkg/cmd/apikeys/apikeys.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/algolia/cli/pkg/cmd/apikeys/create"
 	"github.com/algolia/cli/pkg/cmd/apikeys/delete"
+	"github.com/algolia/cli/pkg/cmd/apikeys/get"
 	"github.com/algolia/cli/pkg/cmd/apikeys/list"
 	"github.com/algolia/cli/pkg/cmdutil"
 )
@@ -20,6 +21,7 @@ func NewAPIKeysCmd(f *cmdutil.Factory) *cobra.Command {
 	cmd.AddCommand(list.NewListCmd(f, nil))
 	cmd.AddCommand(create.NewCreateCmd(f, nil))
 	cmd.AddCommand(delete.NewDeleteCmd(f, nil))
+	cmd.AddCommand(get.NewGetCmd(f, nil))
 
 	return cmd
 }
---
+++ pkg/cmd/apikeys/get/get.go
@@ -0,0 +1,98 @@
+package get
+
+import (
+	"fmt"
+
+	"github.com/MakeNowJust/heredoc"
+	"github.com/algolia/algoliasearch-client-go/v3/algolia/search"
+	"github.com/spf13/cobra"
+
+	"github.com/algolia/cli/pkg/cmd/apikeys/shared"
+	"github.com/algolia/cli/pkg/cmdutil"
+	"github.com/algolia/cli/pkg/config"
+	"github.com/algolia/cli/pkg/iostreams"
+	"github.com/algolia/cli/pkg/validators"
+)
+
+// GetOptions represents the options for the get command
+type GetOptions struct {
+	config config.IConfig
+	IO     *iostreams.IOStreams
+
+	SearchClient func() (*search.Client, error)
+
+	APIKey string
+
+	PrintFlags *cmdutil.PrintFlags
+}
+
+// NewGetCmd returns a new instance of DeleteCmd
+func NewGetCmd(f *cmdutil.Factory, runF func(*GetOptions) error) *cobra.Command {
+	opts := &GetOptions{
+		IO:           f.IOStreams,
+		config:       f.Config,
+		SearchClient: f.SearchClient,
+		PrintFlags:   cmdutil.NewPrintFlags().WithDefaultOutput("json"),
+	}
+
+	cmd := &cobra.Command{
+		Use:   "get <api-key>",
+		Short: "Get API key",
+		Long: heredoc.Doc(`
+			Get the details of a given API Key (ACLs, description, indexes, and other attributes).
+		`),
+		Example: heredoc.Doc(`
+			# Get an API key
+			$ algolia --application-id app-id apikeys get abcdef1234567890
+		`),
+		Args: validators.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			opts.APIKey = args[0]
+
+			if runF != nil {
+				return runF(opts)
+			}
+
+			return runGetCmd(opts)
+		},
+	}
+
+	return cmd
+}
+
+// runGetCmd runs the get command
+func runGetCmd(opts *GetOptions) error {
+	opts.config.Profile().APIKey = opts.APIKey
+	client, err := opts.SearchClient()
+	if err != nil {
+		return err
+	}
+
+	key, err := client.GetAPIKey(opts.APIKey)
+	if err != nil {
+		return fmt.Errorf("API key %q does not exist", opts.APIKey)
+	}
+
+	p, err := opts.PrintFlags.ToPrinter()
+	if err != nil {
+		return err
+	}
+	keyResult := shared.JSONKey{
+		ACL:                    key.ACL,
+		CreatedAt:              key.CreatedAt,
+		Description:            key.Description,
+		Indexes:                key.Indexes,
+		MaxQueriesPerIPPerHour: key.MaxQueriesPerIPPerHour,
+		MaxHitsPerQuery:        key.MaxHitsPerQuery,
+		Referers:               key.Referers,
+		QueryParameters:        key.QueryParameters,
+		Validity:               key.Validity,
+		Value:                  key.Value,
+	}
+
+	if err := p.Print(opts.IO, keyResult); err != nil {
+		return err
+	}
+
+	return nil
+}
