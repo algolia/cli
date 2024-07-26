3dbe99dc745ab9a33f39c3e7a3a71b9180f95b55
Kai Welke <kai.welke@algolia.com>
2024-07-26 13:47:01 +0200

    feat: browse objects

--- pkg/cmd/objects/browse/browse.go
+++ pkg/cmd/objects/browse/browse.go
@@ -1,11 +1,13 @@
 package browse
 
 import (
-	"io"
+	"errors"
+	"fmt"
+	"reflect"
+	"unicode"
 
 	"github.com/MakeNowJust/heredoc"
-	"github.com/algolia/algoliasearch-client-go/v3/algolia/opt"
-	"github.com/algolia/algoliasearch-client-go/v3/algolia/search"
+	"github.com/algolia/algoliasearch-client-go/v4/algolia/search"
 	"github.com/spf13/cobra"
 
 	"github.com/algolia/cli/pkg/cmdutil"
@@ -18,10 +20,11 @@ type BrowseOptions struct {
 	Config config.IConfig
 	IO     *iostreams.IOStreams
 
-	SearchClient func() (*search.Client, error)
+	SearchClient func() (*search.APIClient, error)
 
-	Indice       string
+	Index        string
 	BrowseParams map[string]interface{}
+	Retries      int
 
 	PrintFlags *cmdutil.PrintFlags
 }
@@ -31,14 +34,14 @@ func NewBrowseCmd(f *cmdutil.Factory) *cobra.Command {
 	opts := &BrowseOptions{
 		IO:           f.IOStreams,
 		Config:       f.Config,
-		SearchClient: f.SearchClient,
+		SearchClient: f.V4_SearchClient,
 		PrintFlags:   cmdutil.NewPrintFlags().WithDefaultOutput("json"),
 	}
 
 	cmd := &cobra.Command{
 		Use:               "browse <index>",
 		Args:              validators.ExactArgs(1),
-		ValidArgsFunction: cmdutil.IndexNames(opts.SearchClient),
+		ValidArgsFunction: cmdutil.V4_IndexNames(opts.SearchClient),
 		Annotations: map[string]string{
 			"runInWebCLI": "true",
 			"acls":        "browse",
@@ -61,7 +64,7 @@ func NewBrowseCmd(f *cmdutil.Factory) *cobra.Command {
 			$ algolia objects browse MOVIES > movies.ndjson
 		`),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			opts.Indice = args[0]
+			opts.Index = args[0]
 
 			browseParams, err := cmdutil.FlagValuesMap(cmd.Flags(), cmdutil.BrowseParamsObject...)
 			if err != nil {
@@ -72,6 +75,9 @@ func NewBrowseCmd(f *cmdutil.Factory) *cobra.Command {
 			return runBrowseCmd(opts)
 		},
 	}
+
+	cmd.Flags().
+		IntVarP(&opts.Retries, "retries", "r", 1000, "Max. number of browse requests. Each request retrieves up to 1,000 records.")
 
 	cmd.SetUsageFunc(cmdutil.UsageFuncWithInheritedFlagsOnly(f.IOStreams, cmd))
 
@@ -87,36 +93,69 @@ func runBrowseCmd(opts *BrowseOptions) error {
 		return err
 	}
 
-	indice := client.InitIndex(opts.Indice)
-
-	// We use the `opt.ExtraOptions` to pass the `SearchParams` to the API.
-	query, ok := opts.BrowseParams["query"].(string)
-	if !ok {
-		query = ""
-	} else {
-		delete(opts.BrowseParams, "query")
-	}
-	res, err := indice.BrowseObjects(opt.Query(query), opt.ExtraOptions(opts.BrowseParams))
-	if err != nil {
-		return err
-	}
+	browseParams := search.NewEmptyBrowseParamsObject()
+	// Convert `v3` options to `v4`
+	MapToStruct(opts.BrowseParams, browseParams)
 
 	p, err := opts.PrintFlags.ToPrinter()
 	if err != nil {
 		return err
 	}
 
-	for {
-		iObject, err := res.Next()
-		if err != nil {
-			if err == io.EOF {
-				return nil
+	_, err = client.BrowseObjects(
+		opts.Index,
+		*browseParams,
+		search.WithAggregator(func(res any, _ error) {
+			for _, hit := range res.(*search.BrowseResponse).Hits {
+				p.Print(opts.IO, hit)
 			}
-			return err
-		}
-		if err = p.Print(opts.IO, iObject); err != nil {
-			return err
+		}),
+		search.WithMaxRetries(opts.Retries),
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Capitalize makes the first letter of a word uppercase
+func Capitalize(word string) string {
+	if len(word) == 0 {
+		return word
+	}
+	firstRune := []rune(word)[0]
+	rest := []rune(word)[1:]
+	return string(unicode.ToUpper(firstRune)) + string(rest)
+}
+
+// MapToStruct converts a map into a struct
+func MapToStruct(m map[string]any, s interface{}) error {
+	val := reflect.ValueOf(s).Elem()
+
+	for k, v := range m {
+		// cmdline options are lowercase (`--query`),
+		// but struct fields are capital (`Query`)
+		field := val.FieldByName(Capitalize(k))
+		if !field.IsValid() {
+			return errors.New(fmt.Sprintf("No such parameter: %s for browse\n.", k))
 		}
 
+		if !field.CanSet() {
+			return errors.New(fmt.Sprintf("Can't set field: %s\n", field))
+		}
+
+		fieldValue := reflect.ValueOf(v)
+
+		if field.Type().Kind() == reflect.Ptr &&
+			fieldValue.Type().ConvertibleTo(field.Type().Elem()) {
+			newValue := reflect.New(fieldValue.Type()).Elem()
+			newValue.Set(fieldValue)
+			field.Set(newValue.Addr())
+		} else if fieldValue.Type().ConvertibleTo(field.Type()) {
+			field.Set(fieldValue.Convert(field.Type()))
+		} else {
+			return errors.New(fmt.Sprintf("Can't convert type of %s to %s\n", fieldValue.Type(), field.Type()))
+		}
 	}
+	return nil
 }
