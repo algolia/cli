2ca0618264c20435bc2ef5c5a164b0a22a7d4898
Kai Welke <kai.welke@algolia.com>
2024-07-29 11:00:01 +0200

    fix: refactor map_to_struct

--- pkg/cmd/objects/browse/browse.go
+++ pkg/cmd/objects/browse/browse.go
@@ -1,11 +1,6 @@
 package browse
 
 import (
-	"errors"
-	"fmt"
-	"reflect"
-	"unicode"
-
 	"github.com/MakeNowJust/heredoc"
 	"github.com/algolia/algoliasearch-client-go/v4/algolia/search"
 	"github.com/spf13/cobra"
@@ -95,7 +90,7 @@ func runBrowseCmd(opts *BrowseOptions) error {
 
 	browseParams := search.NewEmptyBrowseParamsObject()
 	// Convert `v3` options to `v4`
-	MapToStruct(opts.BrowseParams, browseParams)
+	cmdutil.MapToStruct(opts.BrowseParams, browseParams)
 
 	p, err := opts.PrintFlags.ToPrinter()
 	if err != nil {
@@ -114,48 +109,6 @@ func runBrowseCmd(opts *BrowseOptions) error {
 	)
 	if err != nil {
 		return err
-	}
-	return nil
-}
-
-// Capitalize makes the first letter of a word uppercase
-func Capitalize(word string) string {
-	if len(word) == 0 {
-		return word
-	}
-	firstRune := []rune(word)[0]
-	rest := []rune(word)[1:]
-	return string(unicode.ToUpper(firstRune)) + string(rest)
-}
-
-// MapToStruct converts a map into a struct
-func MapToStruct(m map[string]any, s interface{}) error {
-	val := reflect.ValueOf(s).Elem()
-
-	for k, v := range m {
-		// cmdline options are lowercase (`--query`),
-		// but struct fields are capital (`Query`)
-		field := val.FieldByName(Capitalize(k))
-		if !field.IsValid() {
-			return errors.New(fmt.Sprintf("No such parameter: %s for browse\n.", k))
-		}
-
-		if !field.CanSet() {
-			return errors.New(fmt.Sprintf("Can't set field: %s\n", field))
-		}
-
-		fieldValue := reflect.ValueOf(v)
-
-		if field.Type().Kind() == reflect.Ptr &&
-			fieldValue.Type().ConvertibleTo(field.Type().Elem()) {
-			newValue := reflect.New(fieldValue.Type()).Elem()
-			newValue.Set(fieldValue)
-			field.Set(newValue.Addr())
-		} else if fieldValue.Type().ConvertibleTo(field.Type()) {
-			field.Set(fieldValue.Convert(field.Type()))
-		} else {
-			return errors.New(fmt.Sprintf("Can't convert type of %s to %s\n", fieldValue.Type(), field.Type()))
-		}
 	}
 	return nil
 }
--- pkg/cmd/search/search.go
+++ pkg/cmd/search/search.go
@@ -1,11 +1,6 @@
 package search
 
 import (
-	"errors"
-	"fmt"
-	"reflect"
-	"unicode"
-
 	"github.com/MakeNowJust/heredoc"
 	"github.com/algolia/algoliasearch-client-go/v4/algolia/search"
 	"github.com/spf13/cobra"
@@ -95,7 +90,7 @@ func runSearchCmd(opts *SearchOptions) error {
 	}
 	searchParams := search.NewEmptySearchParamsObject()
 	// Convert `v3` options to `v4`
-	MapToStruct(opts.SearchParams, searchParams)
+	cmdutil.MapToStruct(opts.SearchParams, searchParams)
 
 	p, err := opts.PrintFlags.ToPrinter()
 	if err != nil {
@@ -119,46 +114,4 @@ func runSearchCmd(opts *SearchOptions) error {
 	opts.IO.StopProgressIndicator()
 
 	return p.Print(opts.IO, res)
-}
-
-// Capitalize makes the first letter of a word uppercase
-func Capitalize(word string) string {
-	if len(word) == 0 {
-		return word
-	}
-	firstRune := []rune(word)[0]
-	rest := []rune(word)[1:]
-	return string(unicode.ToUpper(firstRune)) + string(rest)
-}
-
-// MapToStruct converts a map into a struct
-func MapToStruct(m map[string]any, s interface{}) error {
-	val := reflect.ValueOf(s).Elem()
-
-	for k, v := range m {
-		// cmdline options are lowercase (`--query`),
-		// but struct fields are capital (`Query`)
-		field := val.FieldByName(Capitalize(k))
-		if !field.IsValid() {
-			return errors.New(fmt.Sprintf("No such parameter: %s for browse\n.", k))
-		}
-
-		if !field.CanSet() {
-			return errors.New(fmt.Sprintf("Can't set field: %s\n", field))
-		}
-
-		fieldValue := reflect.ValueOf(v)
-
-		if field.Type().Kind() == reflect.Ptr &&
-			fieldValue.Type().ConvertibleTo(field.Type().Elem()) {
-			newValue := reflect.New(fieldValue.Type()).Elem()
-			newValue.Set(fieldValue)
-			field.Set(newValue.Addr())
-		} else if fieldValue.Type().ConvertibleTo(field.Type()) {
-			field.Set(fieldValue.Convert(field.Type()))
-		} else {
-			return errors.New(fmt.Sprintf("Can't convert type of %s to %s\n", fieldValue.Type(), field.Type()))
-		}
-	}
-	return nil
 }
