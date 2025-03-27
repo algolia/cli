330ca28292629dac477158c892143b4456915d46
Kai Welke <kai.welke@algolia.com>
2025-03-27 10:19:52 +0100

    feat: add end-to-end tests (#179)
    * feat(e2e): test for version
    
    Setup the end-to-end test machinery and add a simple test for the version command
    
    * feat(e2e): index commands
    
    * feat(e2e): settings tests
    
    * feat(e2e): objects commands
    
    * feat(e2e): synonyms commands
    
    * feat(e2e): rules commands
    
    * fixuxp synoynms
    
    * feat(e2e): search commands
    
    * chore: go mod tidy
    
    * chore: update e2e README
    
    * fix(e2e): replace hard-coded index name

---
+++ e2e/e2e_test.go
@@ -0,0 +1,150 @@
+//go:build e2e
+
+package e2e_test
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/algolia/cli/pkg/cmd/root"
+	"github.com/cli/go-internal/testscript"
+)
+
+// algolia runs the root command of the Algolia CLI
+func algolia() int {
+	return int(root.Execute())
+}
+
+// TestMain sets the executable program so that we don't depend on the compiled binary
+func TestMain(m *testing.M) {
+	os.Exit(testscript.RunMain(m, map[string]func() int{
+		"algolia": algolia,
+	}))
+}
+
+// testEnvironment stores the environment variables we need to setup for the tests
+type testEnvironment struct {
+	AppID  string
+	ApiKey string
+}
+
+// getEnv reads the environment variables and prints errors for missing ones
+func (e *testEnvironment) getEnv() error {
+	env := map[string]string{}
+
+	required := []string{
+		// The CLI testing Algolia app
+		"ALGOLIA_APPLICATION_ID",
+		// API key with sufficient permissions to run all tests
+		"ALGOLIA_API_KEY",
+	}
+
+	var missing []string
+
+	for _, envVar := range required {
+		val, ok := os.LookupEnv(envVar)
+		if val == "" || !ok {
+			missing = append(missing, envVar)
+			continue
+		}
+
+		env[envVar] = val
+	}
+
+	if len(missing) > 0 {
+		return fmt.Errorf("missing environment variables: %s", strings.Join(missing, ", "))
+	}
+
+	e.AppID = env["ALGOLIA_APPLICATION_ID"]
+	e.ApiKey = env["ALGOLIA_API_KEY"]
+
+	return nil
+}
+
+// For the `defer` function
+var keyT struct{}
+
+// setupEnv sets up the environment variables for the test
+func setupEnv(testEnv testEnvironment) func(ts *testscript.Env) error {
+	return func(ts *testscript.Env) error {
+		ts.Setenv("ALGOLIA_APPLICATION_ID", testEnv.AppID)
+		ts.Setenv("ALGOLIA_API_KEY", testEnv.ApiKey)
+
+		ts.Values[keyT] = ts.T()
+		return nil
+	}
+}
+
+// setupCmds sets up custom commands we want to make available in the test scripts
+func setupCmds(
+	testEnv testEnvironment,
+) map[string]func(ts *testscript.TestScript, neg bool, args []string) {
+	return map[string]func(ts *testscript.TestScript, neg bool, args []string){
+		"defer": func(ts *testscript.TestScript, neg bool, args []string) {
+			if neg {
+				ts.Fatalf("unsupported ! defer")
+			}
+			tt, ok := ts.Value(keyT).(testscript.T)
+			if !ok {
+				ts.Fatalf("%v is not a testscript.T", ts.Value(keyT))
+			}
+			ts.Defer(func() {
+				if err := ts.Exec(args[0], args[1:]...); err != nil {
+					tt.FailNow()
+				}
+			})
+		},
+	}
+}
+
+// runTestsInDir runs all test scripts from a directory
+func runTestsInDir(t *testing.T, dirName string) {
+	var testEnv testEnvironment
+	if err := testEnv.getEnv(); err != nil {
+		t.Fatal(err)
+	}
+	t.Parallel()
+	t.Log("Running e2e tests in", dirName)
+	testscript.Run(t, testscript.Params{
+		Dir:   dirName,
+		Setup: setupEnv(testEnv),
+		Cmds:  setupCmds(testEnv),
+	})
+}
+
+// TestVersion tests the version option
+func TestVersion(t *testing.T) {
+	runTestsInDir(t, "testscripts/version")
+}
+
+// TestIndices test `algolia indices` commands
+func TestIndices(t *testing.T) {
+	runTestsInDir(t, "testscripts/indices")
+}
+
+// TestSettings tests `algolia settings` commands
+func TestSettings(t *testing.T) {
+	runTestsInDir(t, "testscripts/settings")
+}
+
+// TestObjects tests `algolia objects` commands
+func TestObjects(t *testing.T) {
+	runTestsInDir(t, "testscripts/objects")
+}
+
+// TestSynonyms tests `algolia synonyms` commands
+func TestSynonyms(t *testing.T) {
+	runTestsInDir(t, "testscripts/synonyms")
+}
+
+// TestRules tests `algolia rules` commands
+func TestRules(t *testing.T) {
+	runTestsInDir(t, "testscripts/rules")
+}
+
+// TestSearch tests `algolia search`
+func TestSearch(t *testing.T) {
+	runTestsInDir(t, "testscripts/search")
+}
